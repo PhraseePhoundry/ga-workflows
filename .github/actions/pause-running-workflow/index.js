b6821e7165c4f98987095c81b5674e573d07e73c
Emily Birch <emily.birch@phrasee.co>
2022-11-28 15:10:58 +0000

    added timer interval to pause workflow

--- .github/actions/pause-running-workflow/index.js
+++ .github/actions/pause-running-workflow/index.js
@@ -1,39 +1,57 @@
 const { Octokit } = require("@octokit/rest");
 
-async function main() {
+const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN
+});
 
-    const event = process.env.GITHUB_EVENT_PATH ? require(process.env.GITHUB_EVENT_PATH) : {};
+function sleep(seconds) {
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+}
 
-    const octokit = new Octokit({
-        auth: process.env.GITHUB_TOKEN
-    });
-
+async function fetchAndConcatenateWorkflowRuns(event) {
+    let queuedWorkflowsList;
+    let inProgressWorkflowsList;
     try {
-        const queuedWorkflowsList = await octokit.rest.actions.listWorkflowRunsForRepo({
+        queuedWorkflowsList = await octokit.rest.actions.listWorkflowRunsForRepo({
             owner: event.repository.owner.login,
             repo: event.repository.name,
             event: 'pull_request',
             status: 'queued',
         })
-        
-        const inProgressWorkflowsList = await octokit.rest.actions.listWorkflowRunsForRepo({
+
+        inProgressWorkflowsList = await octokit.rest.actions.listWorkflowRunsForRepo({
             owner: event.repository.owner.login,
             repo: event.repository.name,
             event: 'pull_request',
             status: 'in_progress',
         })
-        console.log('-------------------------------')
-        console.log(queuedWorkflowsList.data.workflow_runs)
-        console.log('-------------------------------')
-        console.log(inProgressWorkflowsList.data.workflow_runs)
-        const runningWorkflows = (queuedWorkflowsList.data.workflow_runs).concat(inProgressWorkflowsList.data.workflow_runs)
-
-        console.log('-------------------------------')
-        console.log(runningWorkflows)
-
     } catch (err) {
         console.error(`Error retrieving workflows information: ${String(err)}`)
         process.exit(1);
+    }
+
+    return (queuedWorkflowsList.data.workflow_runs).concat(inProgressWorkflowsList.data.workflow_runs)
+}
+
+async function main() {
+    const event = process.env.GITHUB_EVENT_PATH ? require(process.env.GITHUB_EVENT_PATH) : {};
+
+    let runningWorkflows = await fetchAndConcatenateWorkflowRuns(event);
+    console.log('****************')
+    console.log(runningWorkflows)
+    let sleepCounter = 0;
+
+    while (runningWorkflows.length > 0) {
+        console.log('Found workflow runs in progress - pausing...')
+        await sleep(10)
+        sleepCounter += 1
+
+        if (sleepCounter > 30) {
+            console.error('Maximum timeout reached - exiting')
+            process.exit(1)
+        }
+
+        runningWorkflows = await fetchAndConcatenateWorkflowRuns(event)
     }
 
     return
